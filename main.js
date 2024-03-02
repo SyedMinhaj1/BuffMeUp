d08c1259d3444be2da88dff913175b558b427c97
SyedMinhaj1 <156704395+SyedMinhaj1@users.noreply.github.com>
2024-03-02 23:18:47 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    var navbar = document.getElementById("nav");
+    navbar.classList.toggle("show");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var header = document.getElementById("myHeader");
+    header.classList.add("animate-header");
+});
